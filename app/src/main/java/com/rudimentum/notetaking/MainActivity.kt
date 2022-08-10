8103c862db102261f02500e256ec6e7e0e8c6a69
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-10 19:36:47 +0300

    Set Toolbar using binding

--- app/src/main/java/com/rudimentum/notetaking/MainActivity.kt
+++ app/src/main/java/com/rudimentum/notetaking/MainActivity.kt
@@ -2,10 +2,37 @@ package com.rudimentum.notetaking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.rudimentum.notetaking.databinding.ActivityMainBinding
+import com.rudimentum.notetaking.utilities.APP_ACTIVITY
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mToolbar: Toolbar
+    lateinit var mNavController: NavController
+    private var _binding: ActivityMainBinding? = null
+    val mBinding get() = _binding!!
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        APP_ACTIVITY = this
+
+        mToolbar = mBinding.toolbar
+        mNavController = Navigation.findNavController(this, R.id.navHostFragment)
+
+        setSupportActionBar(mToolbar)
+        title = getString(R.string.toolbar_title)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // saving memory
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/rudimentum/notetaking/utilities/Constants.kt
@@ -0,0 +1,5 @@
+package com.rudimentum.notetaking.utilities
+
+import com.rudimentum.notetaking.MainActivity
+
+lateinit var APP_ACTIVITY: MainActivity
