1d0f982181b53b9331f640fe8baaf8f0ae7ec8ae
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-13 21:59:45 +0300

    Create DatabaseRepository.kt

---
+++ app/src/main/java/com/rudimentum/notetaking/database/DatabaseRepository.kt
@@ -0,0 +1,10 @@
+package com.rudimentum.notetaking.database
+
+import androidx.lifecycle.LiveData
+import com.rudimentum.notetaking.models.AppNote
+
+interface DatabaseRepository {
+    val allNotes: LiveData<List<AppNote>>
+    suspend fun insert(note: AppNote, onSuccess: () -> Unit)
+    suspend fun delete(note: AppNote, onSuccess: () -> Unit)
+}
