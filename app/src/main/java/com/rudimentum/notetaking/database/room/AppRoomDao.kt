8c275c2f56bf3552bf549e852c942b4b2035ae4b
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-13 22:00:54 +0300

    Implement Room repository

---
+++ app/src/main/java/com/rudimentum/notetaking/database/room/AppRoomDao.kt
@@ -0,0 +1,17 @@
+package com.rudimentum.notetaking.database.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.rudimentum.notetaking.models.AppNote
+
+@Dao
+interface AppRoomDao {
+    @Query("SELECT *from note_tables")
+    fun getAllNotes(): LiveData<List<AppNote>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(note: AppNote)
+
+    @Delete
+    suspend fun delete(note: AppNote)
+}
---
+++ app/src/main/java/com/rudimentum/notetaking/database/room/AppRoomRepository.kt
@@ -0,0 +1,19 @@
+package com.rudimentum.notetaking.database.room
+
+import androidx.lifecycle.LiveData
+import com.rudimentum.notetaking.database.DatabaseRepository
+import com.rudimentum.notetaking.models.AppNote
+
+class AppRoomRepository(private val appRoomDao: AppRoomDao) : DatabaseRepository {
+    override val allNotes: LiveData<List<AppNote>>
+        get() = appRoomDao.getAllNotes()
+
+    override suspend fun insert(note: AppNote, onSuccess: () -> Unit) {
+        appRoomDao.insert(note)
+    }
+
+    override suspend fun delete(note: AppNote, onSuccess: () -> Unit) {
+        appRoomDao.delete(note)
+    }
+
+}
