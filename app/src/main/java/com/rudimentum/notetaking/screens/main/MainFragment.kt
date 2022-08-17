5bfdae8bc9de6c6b2330bc03aaa67a178ebd21f9
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-17 22:01:32 +0300

    Add viewBinding, clickListener in MainFragment

---
+++ app/src/main/java/com/rudimentum/notetaking/screens/main/MainFragment.kt
@@ -0,0 +1,43 @@
+package com.rudimentum.notetaking.screens.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.rudimentum.notetaking.R
+import com.rudimentum.notetaking.databinding.FragmentMainBinding
+import com.rudimentum.notetaking.utilities.APP_ACTIVITY
+
+class MainFragment : Fragment() {
+
+    private var _binding: FragmentMainBinding? = null
+    private val mBinding get() = _binding!!
+    private lateinit var mViewModel: MainFragmentViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initialization()
+    }
+
+    private fun initialization() {
+        mViewModel = ViewModelProvider(this).get(MainFragmentViewModel::class.java)
+        mBinding.fabAddNote.setOnClickListener {
+            APP_ACTIVITY.mNavController.navigate(R.id.action_mainFragment_to_addNewNoteFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
