c0190c8f31fae0af0593aa647e4102957d827021
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-17 22:00:39 +0300

    Add ViewModel for MainFragment

---
+++ app/src/main/java/com/rudimentum/notetaking/screens/main/MainFragmentViewModel.kt
@@ -0,0 +1,9 @@
+package com.rudimentum.notetaking.screens.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainFragmentViewModel(application: Application): AndroidViewModel(application) {
+    private val context = application
+
+}
