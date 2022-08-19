ab8cd320fbfe4e7c8e03a83b77c7b0b709307557
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-19 20:49:23 +0300

    Change AddNewNoteFragment
    Add string for Toast, change package of AddNewNoteFragment, add ViewBinding, change navigation_graph, setup ViewModel

---
+++ app/src/main/java/com/rudimentum/notetaking/screens/add_new_note/AddNewNoteFragment.kt
@@ -0,0 +1,55 @@
+package com.rudimentum.notetaking.screens.add_new_note
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.rudimentum.notetaking.R
+import com.rudimentum.notetaking.databinding.FragmentAddNewNoteBinding
+import com.rudimentum.notetaking.models.AppNote
+import com.rudimentum.notetaking.utilities.APP_ACTIVITY
+
+class AddNewNoteFragment : Fragment() {
+
+    private var _binding: FragmentAddNewNoteBinding? = null
+    private val mBinding get() = _binding!!
+    private lateinit var mViewModel: AddNewNoteFragmentViewModel
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentAddNewNoteBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initialization()
+    }
+
+    private fun initialization() {
+        mViewModel = ViewModelProvider(this).get(AddNewNoteFragmentViewModel::class.java)
+        mBinding.btnAddNote.setOnClickListener {
+            val name = mBinding.inputNameNote.text.toString()
+            val text = mBinding.inputTextNote.text.toString()
+            if (name.isEmpty()) {
+                Toast.makeText(activity, getString(R.string.toast_message_empty_name_note), Toast.LENGTH_SHORT).show()
+            } else {
+                mViewModel.insert(AppNote(name = name, text = text)) {
+                    APP_ACTIVITY.mNavController.navigate(R.id.action_addNewNoteFragment_to_mainFragment)
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
