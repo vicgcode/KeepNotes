aa2d5c786d0b48c3ff6f047506f1d5c6dcc50073
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-19 20:46:07 +0300

    Add callbacks for Room Repository methods

--- app/src/main/java/com/rudimentum/notetaking/database/room/AppRoomRepository.kt
+++ app/src/main/java/com/rudimentum/notetaking/database/room/AppRoomRepository.kt
@@ -10,10 +10,12 @@ class AppRoomRepository(private val appRoomDao: AppRoomDao) : DatabaseRepository
 
     override suspend fun insert(note: AppNote, onSuccess: () -> Unit) {
         appRoomDao.insert(note)
+        onSuccess()
     }
 
     override suspend fun delete(note: AppNote, onSuccess: () -> Unit) {
         appRoomDao.delete(note)
+        onSuccess()
     }
 
 }
