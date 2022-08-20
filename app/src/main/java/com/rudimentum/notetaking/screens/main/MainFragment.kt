86524bac063c6c19443039f744c81be7a6ac3fa9
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-20 23:41:22 +0300

    Setup displaying and updating list of notes
    Use LiveData and Observer

--- app/src/main/java/com/rudimentum/notetaking/screens/main/MainFragment.kt
+++ app/src/main/java/com/rudimentum/notetaking/screens/main/MainFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.rudimentum.notetaking.R
 import com.rudimentum.notetaking.databinding.FragmentMainBinding
+import com.rudimentum.notetaking.models.AppNote
 
 class MainFragment : Fragment() {
 
     private var _binding: FragmentMainBinding? = null
     private val mBinding get() = _binding!!
     private lateinit var mViewModel: MainFragmentViewModel
+    private lateinit var mRecyclerViewNotesList: RecyclerView
+    private lateinit var mNotesListAdapter: MainNotesListAdapter
+    private lateinit var mObserverList: Observer<List<AppNote>>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +36,17 @@ class MainFragment : Fragment() {
     }
 
     private fun initialization() {
+        mNotesListAdapter = MainNotesListAdapter()
+        mRecyclerViewNotesList = mBinding.rvNotesList
+        mRecyclerViewNotesList.adapter = mNotesListAdapter
+        mObserverList = Observer {
+            // create list and add notes to up of list
+            val listOfNotes = it.asReversed()
+            mNotesListAdapter.setList(listOfNotes)
+        }
+
         mViewModel = ViewModelProvider(this).get(MainFragmentViewModel::class.java)
+        mViewModel.allNotes.observe(this, mObserverList)
         mBinding.fabAddNote.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_addNewNoteFragment)
         }
@@ -39,5 +55,7 @@ class MainFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+        mViewModel.allNotes.removeObserver(mObserverList)
+        mRecyclerViewNotesList.adapter = null
     }
 }
--- app/src/main/java/com/rudimentum/notetaking/screens/main/MainFragmentViewModel.kt
+++ app/src/main/java/com/rudimentum/notetaking/screens/main/MainFragmentViewModel.kt
@@ -2,8 +2,9 @@ package com.rudimentum.notetaking.screens.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.rudimentum.notetaking.utilities.REPOSITORY
 
 class MainFragmentViewModel(application: Application): AndroidViewModel(application) {
-    private val context = application
-
+    // get LiveData from REPOSITORY
+    val allNotes = REPOSITORY.allNotes
 }
