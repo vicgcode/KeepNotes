511d3cb0e04db0b635d5c9d1e607775b24332dc9
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-25 21:57:24 +0300

    Implement note deletion
    Use MenuHost and MenuProvider for add icon for deleting in toolbar. Implement delete() in NoteFragmentViewModel, use Dispatchers.IO for appeal to REPOSITORY and Dispatchers.Main for callback to main thread

--- app/src/main/java/com/rudimentum/notetaking/screens/note/NoteFragment.kt
+++ app/src/main/java/com/rudimentum/notetaking/screens/note/NoteFragment.kt
@@ -1,10 +1,13 @@
 package com.rudimentum.notetaking.screens.note
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.rudimentum.notetaking.R
 import com.rudimentum.notetaking.databinding.FragmentNoteBinding
 import com.rudimentum.notetaking.models.AppNote
@@ -21,7 +24,36 @@ class NoteFragment : Fragment() {
     ): View? {
         _binding = FragmentNoteBinding.inflate(layoutInflater, container, false)
         mCurrentNote = arguments?.getSerializable(R.string.bundle_key_note.toString()) as AppNote
+
         return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+
+        // Add menu items without using the Fragment Menu APIs
+        // Note how we can tie the MenuProvider to the viewLifecycleOwner
+        // and an optional Lifecycle.State (here, RESUMED) to indicate when
+        // the menu should be visible
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // add menu items
+                menuInflater.inflate(R.menu.note_action_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // handle the menu selection
+                return when (menuItem.itemId) {
+                    R.id.btnDelete -> {
+                        mViewModel.delete(mCurrentNote) {
+                            findNavController().navigate(R.id.action_noteFragment_to_mainFragment)
+                        }
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
     override fun onStart() {
@@ -30,8 +62,11 @@ class NoteFragment : Fragment() {
     }
 
     private fun initialization() {
+        // fill textViews from current note
         mBinding.noteName.text = mCurrentNote.name
         mBinding.noteText.text = mCurrentNote.text
+        // initialize view model
+        mViewModel = ViewModelProvider(this).get(NoteFragmentViewModel::class.java)
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/rudimentum/notetaking/screens/note/NoteFragmentViewModel.kt
+++ app/src/main/java/com/rudimentum/notetaking/screens/note/NoteFragmentViewModel.kt
@@ -2,6 +2,21 @@ package com.rudimentum.notetaking.screens.note
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.rudimentum.notetaking.models.AppNote
+import com.rudimentum.notetaking.utilities.REPOSITORY
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class NoteFragmentViewModel(application: Application): AndroidViewModel(application) {
+
+    fun delete(note: AppNote, onSuccess: () -> Unit) {
+        viewModelScope.launch (Dispatchers.IO) {
+            REPOSITORY.delete(note) {
+                viewModelScope.launch (Dispatchers.Main) {
+                    onSuccess()
+                }
+            }
+        }
+    }
 }
