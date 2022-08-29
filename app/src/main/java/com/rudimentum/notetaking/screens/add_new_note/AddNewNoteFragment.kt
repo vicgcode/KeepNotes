09f1d616d93911d5866aafaa331bedbb90969e2b
Victoria <31312528+rudimentum@users.noreply.github.com>
2022-08-29 20:48:31 +0300

    Add action_menu for saving new note
    Delete Button View, create add_new_note_menu

--- app/src/main/java/com/rudimentum/notetaking/screens/add_new_note/AddNewNoteFragment.kt
+++ app/src/main/java/com/rudimentum/notetaking/screens/add_new_note/AddNewNoteFragment.kt
@@ -1,11 +1,12 @@
 package com.rudimentum.notetaking.screens.add_new_note
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.rudimentum.notetaking.R
@@ -32,21 +33,50 @@ class AddNewNoteFragment : Fragment() {
         initialization()
     }
 
-    private fun initialization() {
-        mViewModel = ViewModelProvider(this).get(AddNewNoteFragmentViewModel::class.java)
-        mBinding.btnAddNote.setOnClickListener {
-            val name = mBinding.inputNameNote.text.toString()
-            val text = mBinding.inputTextNote.text.toString()
-            if (name.isEmpty()) {
-                Toast.makeText(activity, getString(R.string.toast_message_empty_name_note), Toast.LENGTH_SHORT).show()
-            } else {
-                mViewModel.insert(AppNote(name = name, text = text)) {
-                    findNavController().navigate(R.id.action_addNewNoteFragment_to_mainFragment)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+
+        // Add menu items without using the Fragment Menu APIs
+        // Note how we can tie the MenuProvider to the viewLifecycleOwner
+        // and an optional Lifecycle.State (here, RESUMED) to indicate when
+        // the menu should be visible
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                // add menu items
+                menuInflater.inflate(R.menu.add_new_note_action_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // handle the menu selection
+                return when (menuItem.itemId) {
+                    R.id.btnSave -> {
+                        handleClickOfAddNewNote()
+                        true
+                    }
+                    else -> false
                 }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
+    private fun initialization() {
+        // initialize view model
+        mViewModel = ViewModelProvider(this).get(AddNewNoteFragmentViewModel::class.java)
+    }
+
+    private fun handleClickOfAddNewNote() {
+        // get input data
+        val name = mBinding.inputNameNote.text.toString()
+        val text = mBinding.inputTextNote.text.toString()
+
+        if (name.isEmpty()) {
+            Toast.makeText(activity, getString(R.string.toast_message_empty_name_note), Toast.LENGTH_SHORT).show()
+        } else {
+            mViewModel.insert(AppNote(name = name, text = text)) {
+                findNavController().navigate(R.id.action_addNewNoteFragment_to_mainFragment)
             }
         }
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
